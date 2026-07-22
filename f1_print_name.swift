a6842e3b0b49598f56c5f1176f010cca43ac7a77
oganand <anandog12@gmail.com>
2026-07-22 19:08:36 +0530

    function without parameters

---
+++ f1_print_name.swift
@@ -0,0 +1,10 @@
+// 1.   Basic Function
+
+//    Write a function welcomeStudent(name: String) that returns "Welcome, <name>!". Call it with at least three different names.
+
+func WelcomeStudent(name: String){
+    print("Hello \(name)")
+}
+WelcomeStudent(name: "Anand")
+WelcomeStudent(name: "Hari")
+WelcomeStudent(name: "Fajar")
---
+++ f2_without_parameters.swift
@@ -0,0 +1,9 @@
+// 2.   Function Without Parameters
+
+//   Create a function getInstituteName() that returns the name of your institution as a string. Print the result.
+
+func getInstitutionName() -> String {
+    return ("Our institution is Cusat")
+}
+var name = getInstitutionName()
+print(name)
